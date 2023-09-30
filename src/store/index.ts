00e62327fed8301b0c74ab468ea2ba6ea7cef467
Majo990 <mariajosemonterotroncos@gmail.com>
2023-09-30 13:06:41 -0500

    store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,17 +1,19 @@
 import { createStore } from 'vuex'
 
 
-export default createStore({
-  state: {
-    props: undefined,
-    name:''
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+// import exampleModule from './module-template';
+// import { ExampleStateInterface } from './module-template/state';
+
+
+export interface StateInterface {
+  // Define your own store structure, using submodules if needed
+  // example: ExampleStateInterface;
+  // Declared as unknown to avoid linting issue. Best to strongly type as per the line above.
+   example: unknown
+}
+
+export default createStore<StateInterface>({
   modules: {
+     example: {}
   }
 })
